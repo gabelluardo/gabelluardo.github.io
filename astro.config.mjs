29abe008ffdb7c0bdb2f3ee58430b0efbdb1d07b
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2023-07-18 19:27:29 +0200

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import purgecss from "astro-purgecss";
-// import partytown from "@astrojs/partytown";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gabelluardo.github.io",
   integrations: [
-    // partytown({
-    //   config: {
-    //     forward: ["dataLayer.push"],
-    //   },
-    // }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+        debug: false,
+      },
+    }),
     purgecss({
       fontFace: true,
       keyframes: true,
