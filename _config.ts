850fcc4a5cdf09f7ceb354ea34e66f7a351c8756
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2025-05-13 00:05:48 +0200

    bump lume v3.0.1

--- _config.ts
+++ _config.ts
@@ -1,10 +1,12 @@
 import lume from "lume/mod.ts";
 import mdx from "lume/plugins/mdx.ts";
+import jsx from "lume/plugins/jsx.ts";
 import sass from "lume/plugins/sass.ts";
 import metas from "lume/plugins/metas.ts";
 import robots from "lume/plugins/robots.ts";
-import jsx from "lume/plugins/jsx_preact.ts";
+import brotli from "lume/plugins/brotli.ts";
 import favicon from "lume/plugins/favicon.ts";
+import purgecss from "lume/plugins/purgecss.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
 
@@ -18,20 +20,25 @@ const site = lume({
 });
 
 site
-  .use(jsx())
   .use(mdx())
   .use(sass())
   .use(metas())
+  .use(brotli())
   .use(favicon())
+  .use(purgecss())
   .use(minifyHTML())
   .use(lightningCss())
   .use(
     robots(
       { allow: ["Googlebot", "Bingbot", "DuckDuckBot"], disallow: ["*"] },
     ),
-  );
+  ).use(jsx({
+    pageSubExtension: "", // Reverts to Lume 2 behavior
+  }));
 
-site.copy("site.webmanifest");
+site
+  .add("main.scss")
+  .add("site.webmanifest");
 
 // tranform all links to open in a new tab
 site.process([".html"], (pages) => {
